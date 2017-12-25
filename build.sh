418ccfcc49b194547e391e3ba3568574af00766b
Ppamo <pablo@ppamo.cl>
2017-12-25 12:08:47 -0300

    Update versioning system adding autoincrement of patch number

--- build.sh
+++ build.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
+VERSION=0.1.0
 BINARYFILE=docker/noisy
 DOMAIN=ppamo.cl
 APPNAME=noisy
 
-get_md5sum(){
-	unset MD5SUM
-	if [ -f "$1" ]
-	then
-		md5sum "$1" | awk '{ print $1 }'
-	fi
-}
-
-ORIGINALSIGNATURE=$(get_md5sum $BINARYFILE)
 rm -f $BINARYFILE
 
 echo "* Building noisy"
@@ -23,15 +15,17 @@ then
 	if [ -x $BINARYFILE ]
 	then
 		VERSION=$(docker images | grep "$DOMAIN/$APPNAME" | awk '{ print $2 }')
-		if [ -z "$VERSION" ]
+		if [ "$VERSION" ]
 		then
-			VERSION="1.0.0"
-		else
 			echo "* Deleting image $DOMAIN/$APPNAME:$VERSION"
 			docker rmi $DOMAIN/$APPNAME:$VERSION
 		fi
-		SIGNATURE=$(get_md5sum $BINARYFILE)
-		echo "=> comparing md5sum $ORIGINALSIGNATURE ? $SIGNATURE"
+		TAGVERSION=$(git tag | grep -E "^v[0-9]+\.[0-9]+\.[0-9]+$" | tail -n 1)
+		if [ "$TAGVERSION" ]
+		then
+			PATCHNUMBER=$(echo "$TAGVERSION" | grep -Eo "[0-9]+$")
+			VERSION=${TAGVERSION%$PATCHNUMBER}$((PATCHNUMBER+1))
+		fi
 		echo "* Building docker image $DOMAIN/$APPNAME:$VERSION"
 		docker build -t $DOMAIN/$APPNAME:$VERSION docker/
 		exit 0
