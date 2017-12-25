e6aab937cc756aa32a50aa83c489267a0f98f4f7
Ppamo <pablo@ppamo.cl>
2017-12-25 12:44:09 -0300

    Add color to script output

--- build.sh
+++ build.sh
@@ -4,11 +4,14 @@ VERSION=0.1.0
 BINARYFILE=docker/noisy
 DOMAIN=ppamo.cl
 APPNAME=noisy
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
 
 rm -f $BINARYFILE
 
-echo "* Building noisy"
-go get && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o $BINARYFILE .
+printf "${ORANGE}* Building noisy ${NC}\n"
+go get && CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o $BINARYFILE .
 
 if [ $? -eq 0 ]
 then
@@ -17,7 +20,7 @@ then
 		VERSION=$(docker images | grep "$DOMAIN/$APPNAME" | awk '{ print $2 }')
 		if [ "$VERSION" ]
 		then
-			echo "* Deleting image $DOMAIN/$APPNAME:$VERSION"
+			printf "${ORANGE}* Deleting image $DOMAIN/$APPNAME:$VERSION ${NC}\n"
 			docker rmi $DOMAIN/$APPNAME:$VERSION
 		fi
 		TAGVERSION=$(git tag | grep -E "^v[0-9]+\.[0-9]+\.[0-9]+$" | tail -n 1)
@@ -26,16 +29,16 @@ then
 			PATCHNUMBER=$(echo "$TAGVERSION" | grep -Eo "[0-9]+$")
 			VERSION=${TAGVERSION%$PATCHNUMBER}$((PATCHNUMBER+1))
 		fi
-		echo "* Building docker image $DOMAIN/$APPNAME:$VERSION"
+		printf "${ORANGE}* Building docker image $DOMAIN/$APPNAME:$VERSION ${NC}\n"
 		docker build -t $DOMAIN/$APPNAME:$VERSION docker/
 		IMAGESNUMBER=$(docker images $DOMAIN/$APPNAME:$VERSION --format "{{.ID}}" | wc -l)
 		if [ $IMAGESNUMBER -gt 0 ]
 		then
-			echo "* Running container"
+			printf "${ORANGE}* Running container ${NC}\n"
 			docker run --rm=true -ti $DOMAIN/$APPNAME:$VERSION
 		fi
 		exit 0
 	fi
 fi
-echo "Filed!!"
+printf  "${RED}* Build iled!!${NC}\n"
 exit 1
