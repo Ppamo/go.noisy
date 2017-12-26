e1694786b83ea04d8e8c00afd8c2ebc3cc41906f
Pablo Mansilla Ojeda <pablo@ppamo.cl>
2017-12-25 23:57:30 -0300

    Make it works in cygwin

--- build.sh
+++ build.sh
@@ -3,12 +3,16 @@ DOCKERIMAGE="${DOCKERIMAGE:-docker.io/golang:1.9.2}"
 SRC=$1
 DST=$2
 
-CMD="CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o \"$DST\" \"$SRC\""
+CMD="cd \"src/$SRC\" && go get && CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o \"$DST\" ."
 
-which chcon > /dev/null 2>&1
-if [ $? -eq 0 ]
+OS=$(uname -o)
+if [ "$OS" != "Cygwin" ]
 then
-	chcon -Rt svirt_sandbox_file_t $GOPATH
+	which chcon > /dev/null 2>&1
+	if [ $? -eq 0 ]
+	then
+		chcon -Rt svirt_sandbox_file_t $GOPATH
+	fi
 fi
 
-docker run --rm --privileged=true -ti -v "$GOPATH:/go" "$DOCKERIMAGE" /bin/bash -c "$CMD"
+docker run --rm --privileged=true -i -v "$GOPATH:/go" "$DOCKERIMAGE" /bin/bash -c "$CMD"
